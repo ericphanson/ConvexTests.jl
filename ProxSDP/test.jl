6b696444ef965a30b2a3f2c9f11f5b28ca04f1d6
Eric <5846501+ericphanson@users.noreply.github.com>
2020-02-28 23:08:52 -0600

    Wrap everything in one testset

--- ProxSDP/test.jl
+++ ProxSDP/test.jl
@@ -1,7 +1,9 @@
-using Convex, ProxSDP
+using Convex, ProxSDP, Test
 using Convex.ProblemDepot: run_tests
 
 @info "Starting ProxSDP tests"
-run_tests(; exclude=[r"mip"]) do p
-    solve!(p, () -> ProxSDP.Optimizer(tol_primal=1e-6, tol_dual=1e-6))
+@testset "ProxSDP tests" begin
+    run_tests(; exclude=[r"mip"]) do p
+        solve!(p, () -> ProxSDP.Optimizer(tol_primal=1e-6, tol_dual=1e-6))
+    end
 end
--- SCS/test.jl
+++ SCS/test.jl
@@ -1,7 +1,9 @@
-using Convex, SCS
+using Convex, SCS, Test
 using Convex.ProblemDepot: run_tests
 
 @info "Starting SCS tests"
-run_tests(; exclude=[r"mip"]) do p
-    solve!(p, () -> SCS.Optimizer(verbose=0, eps=1e-6))
+@testset "SCS tests" begin
+    run_tests(; exclude=[r"mip"]) do p
+        solve!(p, () -> SCS.Optimizer(verbose=0, eps=1e-6))
+    end
 end
