491edabd7595c8a4f4b523fbe1063cbcab38044f
Eric <5846501+ericphanson@users.noreply.github.com>
2020-02-29 00:28:24 -0600

    test

--- SCS/test.jl
+++ SCS/test.jl
@@ -1,7 +1,7 @@
 using Convex, SCS, Test
 using Convex.ProblemDepot: run_tests
 
-@info "Starting SCS tests"
+@info "Starting SCS tests."
 @testset "SCS tests" begin
     run_tests(; exclude=[r"mip"]) do p
         solve!(p, () -> SCS.Optimizer(verbose=0, eps=1e-6))
