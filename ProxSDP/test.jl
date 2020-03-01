4c79d0bfec6b5f3a6379f85ab3abf91d47f2118e
Eric <5846501+ericphanson@users.noreply.github.com>
2020-02-29 23:29:25 -0600

    Add ProxSDP; try to fix docs

--- ProxSDP/test.jl
+++ ProxSDP/test.jl
@@ -1,9 +1,4 @@
-using Convex, ProxSDP, Test
-using Convex.ProblemDepot: run_tests
+using ConvexTests, ProxSDP
 
 @info "Starting ProxSDP tests"
-@testset "ProxSDP tests" begin
-    run_tests(; exclude=[r"mip"]) do p
-        solve!(p, () -> ProxSDP.Optimizer(tol_primal=1e-6, tol_dual=1e-6))
-    end
-end
+do_tests("ProxSDP", () -> ProxSDP.Optimizer(tol_primal=1e-6, tol_dual=1e-6); exclude = [r"mip", r"socp", r"exp", r"affine"])
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ConvexTests
 
 [![Build Status](https://github.com/ericphanson/ConvexTests.jl/workflows/CI/badge.svg)](https://github.com/ericphanson/ConvexTests.jl/actions)
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://ericphanson.github.io/ConvexTests/stable)
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://ericphanson.github.io/ConvexTests/dev)
