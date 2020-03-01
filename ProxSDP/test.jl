ebdd89dbffa463e856cd0cc50fd3273a8779417f
Eric <5846501+ericphanson@users.noreply.github.com>
2020-03-01 00:03:57 -0600

    Remove `CSDP`, test more problems

--- ProxSDP/test.jl
+++ ProxSDP/test.jl
@@ -1,4 +1,4 @@
 using ConvexTests, ProxSDP
 
 @info "Starting ProxSDP tests"
-do_tests("ProxSDP", () -> ProxSDP.Optimizer(tol_primal=1e-6, tol_dual=1e-6); exclude = [r"mip", r"socp", r"exp", r"affine"])
+do_tests("ProxSDP", () -> ProxSDP.Optimizer(tol_primal=1e-6, tol_dual=1e-6); exclude = [r"mip", r"exp"])
--- SCS/test.jl
+++ SCS/test.jl
@@ -2,5 +2,4 @@ using ConvexTests, SCS
 
 @info "Starting SCS tests"
 
-#do_tests("SCS", () -> SCS.Optimizer(verbose=0, eps=1e-6); exclude = [r"mip"])
-do_tests("SCS", () -> SCS.Optimizer(verbose=0, eps=1e-6); exclude = [r"mip", r"socp", r"exp", r"affine"])
+do_tests("SCS", () -> SCS.Optimizer(verbose=0, eps=1e-6); exclude = [r"mip"])
