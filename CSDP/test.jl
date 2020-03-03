63a777ff40cf89a3625c95a6a06bf54f6051882a
Eric <5846501+ericphanson@users.noreply.github.com>
2020-03-03 00:15:36 +0000

    Add SDPA, update files

--- CSDP/test.jl
+++ CSDP/test.jl
@@ -1,5 +1,17 @@
 using ConvexTests, CSDP
 
+using MathOptInterface
+const MOI = MathOptInterface
+using Dualization
+
 @info "Starting CSDP tests"
 
-do_tests("CSDP", () -> CSDP.Optimizer(printlevel=0); exclude = [r"mip", r"socp", r"exp", r"affine"])
+do_tests("CSDP", () -> CSDP.Optimizer(printlevel=0); exclude = [r"mip", r"socp", r"exp"], first = true, last = false)
+
+function dual_opt()
+    opt = DualOptimizer(CSDP.Optimizer(printlevel=0))
+    MOI.set(opt, MOI.Silent(), true)
+    opt
+end
+
+do_tests("CSDP", dual_opt; exclude = [r"mip", r"socp", r"exp"], first = false, last = true, variant="(dualized)")
--- SDPA_GMP/test.jl
+++ SDPA_GMP/test.jl
@@ -1,8 +1,25 @@
 using ConvexTests, SDPAFamily
+using MathOptInterface, Dualization
+const MOI = MathOptInterface
+using GenericLinearAlgebra
 
 @info "Starting SDPA-GMP tests"
 
+do_tests("SDPA_GMP", () -> SDPAFamily.Optimizer(presolve=true, silent=true); exclude = [r"mip", r"exp", r"sdp_Complex_Semidefinite_constraint"], variant="presolve=true", T = BigFloat, description="Tests with SDPA-GMP via SDPAFamily.jl", last = false, first = true)
 
-do_tests("SDPA_GMP", () -> SDPAFamily.Optimizer(presolve=true); exclude = [r"mip", r"exp", r"sdp_Complex_Semidefinite_constraint"], variant="presolve=true", T = BigFloat, description="Tests with SDPA-GMP via SDPAFamily.jl")
+@info "Starting presolve=false tests"
 
-do_tests("SDPA_GMP", () -> SDPAFamily.Optimizer(presolve=false); exclude = [r"mip", r"exp", r"sdp_Complex_Semidefinite_constraint"], variant="presolve=false", append=true, T = BigFloat)
+do_tests("SDPA_GMP", () -> SDPAFamily.Optimizer(presolve=false, silent=true); exclude = [r"mip", r"exp", r"sdp_Complex_Semidefinite_constraint"], variant="presolve=false", first = false, T = BigFloat, last = false)
+
+
+function dual_opt(;kwargs...)
+    () -> begin
+        opt = DualOptimizer(SDPAFamily.Optimizer(kwargs...))
+        MOI.set(opt, MOI.Silent(), true)
+        opt
+    end
+end
+
+@info "Starting dual tests"
+
+do_tests("SDPA_GMP", dual_opt(presolve=true, silent=true); exclude = [r"mip", r"exp", r"sdp_Complex_Semidefinite_constraint"], variant="presolve=true (dualized)", T = BigFloat, description="Tests with SDPA-GMP via SDPAFamily.jl", first = false, last = true)
