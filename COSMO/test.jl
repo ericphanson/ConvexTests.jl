4e7dff3fa6b81540f7cee3af3984a94d1ca98ba3
Eric <5846501+ericphanson@users.noreply.github.com>
2020-03-04 17:58:14 +0000

    Update description for COSMO

--- COSMO/test.jl
+++ COSMO/test.jl
@@ -2,4 +2,4 @@ using ConvexTests, COSMO
 
 @info "Starting COSMO tests"
 
-do_tests("COSMO", () -> COSMO.Optimizer(decompose = false, max_iter = 40000, verbose = false, eps_abs = 5e-7, eps_rel = 5e-7); exclude = [r"mip", r"dual"], description = "Tests run with `eps_abs=1e-6` and `eps_rel=1e-6`.")
+do_tests("COSMO", () -> COSMO.Optimizer(decompose = false, max_iter = 40000, verbose = false, eps_abs = 5e-7, eps_rel = 5e-7); exclude = [r"mip", r"dual"], description = "Tests run with `decompose = false`, `max_iter = 40000`, `eps_abs = 5e-7`, and `eps_rel = 5e-7`.")
