299ec69452f755a5495db810b2ba0a15e6438f55
Joaquim Dias Garcia <joaquimdgarcia@gmail.com>
2020-10-06 06:44:34 -0300

    Update new parameters for ProxSDP (#7)
    Designed to run in 30 minutes with version 1.6.0.

--- ProxSDP/test.jl
+++ ProxSDP/test.jl
@@ -1,4 +1,7 @@
 using ConvexTests, ProxSDP
 
 @info "Starting ProxSDP tests"
-do_tests("ProxSDP", () -> ProxSDP.Optimizer(tol_primal=1e-6, tol_dual=1e-6); exclude = [r"mip", r"exp"], description="Tests run with `tol_primal=1e-6` and `tol_dual=1e-6`.")
+do_tests("ProxSDP", () -> ProxSDP.Optimizer(
+    tol_gap = 5e-8, tol_feasibility = 1e-7, max_iter = 100_000_000, time_limit = 100.0
+); exclude = [r"mip", r"exp"], description =
+"Tests run with `tol_gap = 5e-8`, `tol_feasibility = 1e-7`, `max_iter = 100_000_000` and `time_limit = 100.0`.")
